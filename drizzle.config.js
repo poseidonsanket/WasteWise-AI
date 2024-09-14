e6f29dd9c1e86e038692115059cd91fd92371a85
Sanket Dadali <sanketdadali05@gmail.com>
2024-09-14 18:47:51 +0530

    init and db setup done

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: `${process.env.NEXT_PUBLIC_DATABASE_URL}`,
+    connectionString: `${process.env.NEXT_PUBLIC_DATABASE_URL}`,
+  },
+};
